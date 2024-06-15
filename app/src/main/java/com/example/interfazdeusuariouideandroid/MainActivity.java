7d11184f72e07d4e4d00a699395c37ffdb7550ec
Kiara-y-Josue <wtrianac@uteq.edu.ec>
2024-06-15 14:34:40 -0500

    fouth commit

--- app/src/main/java/com/example/interfazdeusuariouideandroid/MainActivity.java
+++ app/src/main/java/com/example/interfazdeusuariouideandroid/MainActivity.java
@@ -12,7 +12,11 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Pattern email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             idNombre.setHelperText("Ingresado Correctamente");
         }
-
         TextInputLayout idApellido = findViewById(R.id.idApellido);
         TextInputEditText txtApellido = findViewById(R.id.txtApellido);
         String Apellido = txtApellido.getText().toString();
@@ -43,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             idApellido.setHelperText("Ingresado Correctamente");
         }
-
         TextInputLayout idEmpresa = findViewById(R.id.idEmpresa);
         TextInputEditText txtEmpresa = findViewById(R.id.txtEmpresa);
         String Empresa = txtApellido.getText().toString();
@@ -52,14 +54,32 @@ public class MainActivity extends AppCompatActivity {
         } else {
             idEmpresa.setHelperText("Ingresado Correctamente");
         }
-
-
         TextInputLayout idTelefono = findViewById(R.id.idTelefono);
         TextInputEditText txtTelefono = findViewById(R.id.txtTelefono);
         String Telefono = txtTelefono.getText().toString();
         if (Telefono.equals("") || Telefono.length()!=10){
             idTelefono.setError("ingrese una cedula valida");
-        }else
+        }else {
             idTelefono.setHelperText("Ingresado Correctamente");
+        }
+        TextInputLayout idCorreo = findViewById(R.id.idCorreo);
+        TextInputEditText txtCorreo = findViewById(R.id.txtCorreo);
+        String correo = txtCorreo.getText().toString().trim();
+
+        if (correo.equals("") || correo.length() > 100) {
+            idCorreo.setError("Ingrese un correo válido");
+        } else if (!isValidEmail(correo)) {
+            idCorreo.setError("Ingrese un correo electrónico válido");
+        } else {
+            idCorreo.setError(null);
+            idCorreo.setHelperText("Correo electrónico ingresado correctamente");
+        }
+
+
+    }
+
+    private boolean isValidEmail(String email) {
+        String regex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        return email.matches(regex);
     }
 }
